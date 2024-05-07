5c5ee14e3d5610b8943c8b5f6ec5ba36582ab126
krkarma777 <krkarma777@gmail.com>
2024-05-07 10:51:51 +0900

    Add comments to ViewController for better navigation
    Included comments in ViewController to clarify the role of the controller and its method in routing the root URL to the 'main' view. This documentation helps developers understand the flow of the application's front-end routing.

--- src/main/kotlin/com/githubunfollowertracker/controller/ViewController.kt
+++ src/main/kotlin/com/githubunfollowertracker/controller/ViewController.kt
@@ -3,11 +3,13 @@ package com.githubunfollowertracker.controller
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 
+// Controller responsible for handling page view routing
 @Controller
 class ViewController {
 
+    // Mapping for the root URL that loads the main view page
     @GetMapping("/")
     fun showUnfollowPage(): String {
-        return "main"
+        return "main" // Returns the name of the Thymeleaf template to render (main.html)
     }
 }
