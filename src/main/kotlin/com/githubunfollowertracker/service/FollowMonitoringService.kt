46aec5cfa638a16c444629fa93cf4a09a4cd1834
krkarma777 <krkarma777@gmail.com>
2024-05-07 11:53:36 +0900

    Improve FollowMonitoringService with clear comments and documentation

--- src/main/kotlin/com/githubunfollowertracker/service/FollowMonitoringService.kt
+++ src/main/kotlin/com/githubunfollowertracker/service/FollowMonitoringService.kt
@@ -8,22 +8,47 @@ import okhttp3.Request
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
+/**
+ * Service responsible for fetching user following and followers lists from the GitHub API.
+ */
 @Service
 class FollowMonitoringService(private val httpClient: OkHttpClient, private val gson: Gson) {
 
     @Value("\${github.api.url}")
     private lateinit var githubApiUrl: String
 
+    /**
+     * Retrieves the list of users that the specified user is following.
+     *
+     * @param userName The GitHub username
+     * @param credentials The OAuth2 access token for authentication
+     * @return The list of GitHub users being followed
+     */
     fun getFollowingList(userName: String, credentials: String): List<GitHubUser> {
         val url = "$githubApiUrl/users/$userName/following"
         return makeApiCall(url, credentials)
     }
 
+    /**
+     * Retrieves the list of users following the specified user.
+     *
+     * @param userName The GitHub username
+     * @param credentials The OAuth2 access token for authentication
+     * @return The list of GitHub users who are followers
+     */
     fun getFollowersList(userName: String, credentials: String): List<GitHubUser> {
         val url = "$githubApiUrl/users/$userName/followers"
         return makeApiCall(url, credentials)
     }
 
+    /**
+     * Makes an API call to the specified URL with the provided OAuth2 access token.
+     *
+     * @param url The URL to make the API call to
+     * @param credentials The OAuth2 access token for authentication
+     * @return The list of GitHub users returned from the API call
+     * @throws RuntimeException if the API call fails or the response is not successful
+     */
     private fun makeApiCall(url: String, credentials: String): List<GitHubUser> {
         val request = Request.Builder()
             .url(url)
