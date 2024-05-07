802e04878b5923ca29922afe2179f1afc4ea360f
krkarma777 <krkarma777@gmail.com>
2024-05-07 10:47:36 +0900

    Integrate OAuth2 authentication in GitHubController
    This commit updates the GitHubController to use OAuth2 for authentication instead of basic authentication. The unfollow endpoint has been changed from POST to GET and now retrieves user credentials from the OAuth2AuthenticationToken. This ensures secure access to the GitHub API using OAuth2 tokens managed by Spring Security's OAuth2AuthorizedClientService. The method to unfollow non-reciprocal followers has been refined to improve the clarity and efficiency of the logic.

--- src/main/kotlin/com/githubunfollowertracker/controller/GitHubController.kt
+++ src/main/kotlin/com/githubunfollowertracker/controller/GitHubController.kt
@@ -1,22 +1,43 @@
 package com.githubunfollowertracker.controller
 
 import com.githubunfollowertracker.service.GitHubService
-import okhttp3.Credentials
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.*
+import org.springframework.http.ResponseEntity
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class GitHubController @Autowired constructor(private val gitHubService: GitHubService) {
+class GitHubController @Autowired constructor(
+    private val gitHubService: GitHubService,
+    private val clientService: OAuth2AuthorizedClientService
+) {
 
-    @PostMapping("/unfollow")
-    fun unfollow(@RequestParam username: String, @RequestParam password: String, @RequestParam(required = false) whiteList: List<String> = listOf()) {
-        val credentials = Credentials.basic(username, password)
-        var currentPage = 1
-        while (true) {
-            val followed = gitHubService.followedUsers(credentials, currentPage)
-            if (followed.isEmpty()) break
-            gitHubService.unfollowAll(credentials, username, followed, whiteList)
-            currentPage++
-        }
+    @GetMapping("/unfollow")
+    fun unfollow(
+        oAuth2AuthenticationToken: OAuth2AuthenticationToken,
+        @RequestParam(required = false) whiteList: List<String> = listOf()
+    ): ResponseEntity<String> {
+        val credentials = getAccessToken(oAuth2AuthenticationToken) as String
+        val userName = oAuth2AuthenticationToken.principal.attributes["login"] as String
+        val following = gitHubService.fetchFollowing(userName, credentials)
+        val followers = gitHubService.fetchFollowers(userName, credentials)
+
+        following.filterNot { follower -> followers.any { it == follower } }
+            .forEach { userToUnfollow ->
+                gitHubService.unfollowUser(userName, userToUnfollow, credentials)
+            }
+
+        return ResponseEntity.ok("Unfollowed non-followers successfully.")
+    }
+
+    fun getAccessToken(authentication: OAuth2AuthenticationToken): String? {
+        val authorizedClient = clientService.loadAuthorizedClient<OAuth2AuthorizedClient>(
+            "github", authentication.name
+        )
+        return authorizedClient?.accessToken?.tokenValue
     }
 }
