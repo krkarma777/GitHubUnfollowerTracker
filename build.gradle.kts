8f7685fa06e43207d0434b040efb57d52a683828
krkarma777 <krkarma777@gmail.com>
2024-05-08 14:27:04 +0900

    Add kotlinx-coroutines-core dependency for asynchronous processing
    - Added kotlinx-coroutines-core to the dependencies to enable the use of Kotlin coroutines for efficient asynchronous processing and managing concurrent operations in the GitHub unfollow functionality.

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
     implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
 }
 
 tasks.withType<KotlinCompile> {
